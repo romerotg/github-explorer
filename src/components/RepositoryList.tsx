2fe45980bed5fbabb79b0a7c53a0ce60a645b30e
Romero Gonçalves <romerotg@gmail.com>
2021-05-27 13:32:34 -0300

    Switching javascript components to typescript components

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -1,10 +1,17 @@
 import { useState, useEffect } from 'react';
-import { RepositoryItem } from "./RepositoryItem";
+import { RepositoryItem as Repository } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
 
+interface Repository {
+  id: string,
+  name: string,
+  description: string,
+  html_url: string
+}
+
 export function RepositoryList() {
-  const [repositories, setRepositories] = useState([]);
+  const [repositories, setRepositories] = useState<Repository[]>([]);
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -17,7 +24,7 @@ export function RepositoryList() {
       <h1>Lista de Repositórios</h1>
       <ul>
         {repositories.map(repository => {
-          return <RepositoryItem key={repository.id} repository={repository} />
+          return <Repository key={repository.id} repository={repository} />
         })}
       </ul>
     </section>
